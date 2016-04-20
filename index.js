d90987ae5803c9e4fd75ca045e2756ec1147d112
thanatos007 <lmayancela08@gmail.com>
2016-04-20 21:20:48 +0000

    weatherApp

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-window.onload = function () { console.log('loaded!') };
+window.onload = function () {
+	var formElement = document.getElementById('form');
+	var inputElement = document.getElementById('input');
+
+	formElement.onsubmit = function (event) {
+		event.preventDefault();
+		var cityName = inputElement.value;
+		getWeather(cityName);
+	}
+}
+
+function printWeather(e) {
+ var response = JSON.parse(e.target.responseText);
+
+ var node = document.createElement("P");
+    var textnode = document.createTextNode(response.main.temp);
+    node.appendChild(textnode);
+    document.getElementById("weather").appendChild(node);
+}
+
+function getWeather(cityName) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather' +
+  '?appid=c2f2d170f6f6fc336058e9851edb828c' +
+  '&q=' + cityName +
+  '&units=imperial';
+  var xhrObject = new XMLHttpRequest();
+  xhrObject.open('GET', url, true);
+  xhrObject.onload = printWeather;
+  xhrObject.send();
+}
+
+
