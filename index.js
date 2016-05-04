4a31991a8c5dc67954f5e67b4550d37d27a0ee99
thanatos007 <lmayancela08@gmail.com>
2016-05-04 21:16:31 +0000

    weatherApi stuff

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
-window.onload = function () {
-	var formElement = document.getElementById('form');
-	var inputElement = document.getElementById('input');
+// window.onload = function () {
+// 	var formElement = document.getElementById('form');
+// 	var inputElement = document.getElementById('input');
 
-	formElement.onsubmit = function (event) {
-		event.preventDefault();
-		var cityName = inputElement.value;
-		getWeather(cityName);
-	}
-}
+// 	formElement.onsubmit = function (event) {
+// 		event.preventDefault();
+// 		var cityName = inputElement.value;
+// 		getWeather(cityName);
+// 	}
 
-function printWeather(e) {
- var response = JSON.parse(e.target.responseText);
+$(function () {
+    $('form').on('submit', function (event) {
+        event.preventDefault();
+    
+    var userInput = $('input').val();
+    var cities = userInput.split(',');
+    
+    for(var i = 0; i < cities.length; i++){
+        console.log(cities[i]);
+       getWeather(cities[i]);
+    }
+    });
+});
 
- var node = document.createElement("P");
-    var textnode = document.createTextNode(response.main.temp);
-    node.appendChild(textnode);
-    document.getElementById("weather").appendChild(node);
+function printWeather(response) {
+    console.log(response);
+    
+    var stringresponse = JSON.stringify(response);
+    document.write(response.main.temp );
+    //document.write(response.main.temp_min);
+  //var response = JSON.parse(e.target.responseText);
+
+//   var node = document.createElement("P");
+//      var currentWeather = document.createTextNode(response.main.temp)
+//      node.appendChild(currentWeather);
+
 }
 
 function getWeather(cityName) {
-  var url = 'http://api.openweathermap.org/data/2.5/weather' +
+  var url = 'https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather' +
   '?appid=c2f2d170f6f6fc336058e9851edb828c' +
   '&q=' + cityName +
   '&units=imperial';
-  var xhrObject = new XMLHttpRequest();
-  xhrObject.open('GET', url, true);
-  xhrObject.onload = printWeather;
-  xhrObject.send();
+  
+  $.get(url, printWeather);
 }
 
 
